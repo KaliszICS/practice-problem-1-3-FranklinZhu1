7d36780bead6c5ad9b1068e07e1cc1c87358c40a
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-03-06 01:36:40 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,48 @@
+import java.io.*;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static String readFile(String filename) {
+		BufferedReader in = null;
+		try {
+			String ans = "", a;
+			in = new BufferedReader(new FileReader(filename));
+			while ((a = in.readLine()) != null) ans += a + '\n';
+			return ans;
+		}
+		catch (Exception e) {
+			return "";
+		}
+		finally {
+			try {
+				if (in != null) in.close();
+			}
+			catch (Exception e) {
+				return "";
+			}
+		}
 	}
 
-	
+	public static String backwardsReadFile(String filename) {
+		BufferedReader in = null;
+		try {
+			String temp = "", ans = "", a;
+			in = new BufferedReader(new FileReader(filename));
+			while ((a = in.readLine()) != null) temp += a + '\n';
+			for (int i = temp.length() - 1; i >= 0; --i) ans += temp.charAt(i); // there's probably a better way to do this with filereader
+			return ans;
+		}
+		catch (Exception e) {
+			return "";
+		}
+		finally {
+			try {
+				if (in != null) in.close();
+			}
+			catch (Exception e) {
+				return "";
+			}
+		}
+	}
 
 }
